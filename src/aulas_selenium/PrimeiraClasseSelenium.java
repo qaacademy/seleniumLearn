06f9fc8f0a0282e122ca2023d9ce42afb9d4ddcf
qaacademy <qaacademy@gmail.com>
2022-03-29 16:45:28 -0300

    Complemento

--- src/aulas_selenium/PrimeiraClasseSelenium.java
+++ src/aulas_selenium/PrimeiraClasseSelenium.java
@@ -1,20 +1,22 @@
 package aulas_selenium;
 
-import static org.junit.Assert.assertTrue;
+import org.apache.commons.io.FileUtils;
+import org.junit.*;
+import org.junit.rules.TestName;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-import org.junit.*;
-import org.junit.rules.TestName;
-import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import static org.junit.Assert.assertTrue;
 
 public class PrimeiraClasseSelenium {
     @Rule
@@ -25,12 +27,7 @@ public class PrimeiraClasseSelenium {
 
     @Before
     public void iniciaDriver() throws IOException {
-
         inicializarChromeDriver();
-
-        driver = new ChromeDriver();
-        driver.manage().window().setSize(new Dimension(1200, 765));
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS); // Espera 5 segundos
     }
 
     public void testeSelenium() throws InterruptedException {
@@ -102,14 +99,17 @@ public class PrimeiraClasseSelenium {
 
     }
 
-    public void treinandoComFrames() throws InterruptedException {
+    @Test
+    public void treinandoComFrames() throws InterruptedException, IOException {
 
         driver.get("http://demo.automationtesting.in/Frames.html");
         driver.switchTo().frame("singleframe");
         WebElement element = driver.findElement(By.xpath("//*[@type='text']"));
         element.click();
         element.sendKeys("Teste Iframess");
+        screenShot("Teste - Frames");
         driver.switchTo().defaultContent();//Retorna para o contexto da pagina
+        screenShot("Volta contexto");
         Thread.sleep(2000);
 
     }
@@ -187,30 +187,34 @@ public class PrimeiraClasseSelenium {
 //			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+File.pathSeparator+"chromedriver");
 //		}
         ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments(
-                "--headless",
-                "--disable-web-security",
-                "--ignore-certificate-errors",
-                "--allow-running-insecure-content",
-                "--allow-insecure-localhost",
-                "--disable-gpu",
-                "window-size=1200x600",
-                "disable-popup-blocking",
-                "disable-infobars"
-        );
+//        chromeOptions.addArguments(
+//                "--headless",
+//                "--disable-web-security",
+//                "--ignore-certificate-errors",
+//                "--allow-running-insecure-content",
+//                "--allow-insecure-localhost",
+//                "--disable-gpu",
+//                "window-size=1200x600",
+//                "disable-popup-blocking",
+//                "disable-infobars"
+//        );
+        chromeOptions.setHeadless(true); // Seta como headless
+        chromeOptions.setAcceptInsecureCerts(true); // aceita certificados
+        chromeOptions.addArguments("disable-popup-blocking");
+
         driver = new ChromeDriver(chromeOptions);
         driver.manage().window().maximize();
 
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        driver.manage().timeouts().pageLoadTimeout(300, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS); // Timeout Carregameto da Pagina
+        driver.manage().timeouts().pageLoadTimeout(15, TimeUnit.SECONDS);//Timeout carregamento da pagina
 
     }
+
 
     public void screenShot(String nomeScrenShot) throws IOException {
         TakesScreenshot screenShot = ((TakesScreenshot) driver); //Convertendo o Webdriver em TakesScreenshot
         File arquivo = screenShot.getScreenshotAs(OutputType.FILE); //Tira o Screenshot e armazena no File Arquivo
-//        FileUtils.copyFile(arquivo, new File("output" + File.separator + nomeScrenShot + "_screenshot.jpg"));
-
+        FileUtils.copyFile(arquivo, new File("output" + File.separator + nomeScrenShot + "_screenshot.jpg"));//Cria o arquivo .jpg
     }
 
     @After
